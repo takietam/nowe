5f07968b907ad80f4f7d38dfda07d492f942ea79
takietam <error38ms@gmail.com>
2014-01-23 23:34:01 +0100

    Create ustawienia.h

---
+++ ustawienia.h
@@ -0,0 +1,17 @@
+#pragma once
+
+#define znak_weza 254	//	znak reprezentujacy weza
+
+#define szerokosc 45	//	wymiary planszy
+#define wysokosc 20
+
+#define dlug_nick 15	//	dopuszczalna dlugosc nicku
+						//	zmiana moze wymagac zresetowania najlepszych wynikow
+
+#define plik_wyn "wyniki.dat"	//	nazwa pliku przechowujaca najlepsze wyniki
+
+#define ę 169			//	uzywane polskie litery
+#define ś 152
+#define ć 134
+#define ó 162
+#define ł 136
